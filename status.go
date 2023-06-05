bc5256f468805d9c8e130bd66eaa1f77dd650a10
Ahmad Sameh <8120013+asmsh@users.noreply.github.com>
2023-06-05 05:05:53 +0300

    BREAKING: removed any Status function or values related to the 'Pending' state

--- status.go
+++ status.go
@@ -4,30 +4,23 @@ import "github.com/asmsh/promise/internal/status"
 
 // Status holds the state and the fate info of the corresponding Promise,
 // at the time it is created.
-type Status uint32
+type Status status.PromStatus
 
 // State returns the state of the promise.
 //
 // For more info, see 'States and Fates' in the package comment.
 func (s Status) State() string {
-	if status.IsStatePending(uint32(s)) {
-		return "pending"
-	} else if status.IsStateFulfilled(uint32(s)) {
+	switch {
+	case status.IsStateFulfilled(uint32(s)):
 		return "fulfilled"
-	} else if status.IsStateRejected(uint32(s)) {
+	case status.IsStateRejected(uint32(s)):
 		return "rejected"
-	} else if status.IsStatePanicked(uint32(s)) {
+	case status.IsStatePanicked(uint32(s)):
 		return "panicked"
+	default:
+		// only user-created Status values may result in reaching this
+		return "<UnknownState>"
 	}
-	// only user-created Status values may result in reaching this
-	return "<Unknown Promise State>"
-}
-
-// IsPending returns true, only if the state of the promise is Pending.
-//
-// For more info, see 'States and Fates' in the package comment.
-func (s Status) IsPending() bool {
-	return status.IsStatePending(uint32(s))
 }
 
 // IsFulfilled returns true, only if the state of the promise is Fulfilled.
@@ -55,15 +48,17 @@ func (s Status) IsPanicked() bool {
 //
 // For more info, see 'States and Fates' in the package comment.
 func (s Status) Fate() string {
-	if status.IsFateHandled(uint32(s)) {
-		return "handled"
-	} else if status.IsFateResolved(uint32(s)) {
-		return "resolved"
-	} else if status.IsFateResolving(uint32(s)) || status.IsFateUnresolved(uint32(s)) {
+	switch {
+	case status.IsFateResolving(uint32(s)), status.IsFateUnresolved(uint32(s)):
 		return "unresolved"
+	case status.IsFateResolved(uint32(s)):
+		return "resolved"
+	case status.IsFateHandled(uint32(s)):
+		return "handled"
+	default:
+		// only user-created Status values may result in reaching this
+		return "<UnknownFate>"
 	}
-	// only user-created Status values may result in reaching this
-	return "<Unknown Promise Fate>"
 }
 
 // IsResolved returns true, only if the fate of the promise is Resolved or
@@ -74,7 +69,7 @@ func (s Status) IsResolved() bool {
 	return status.IsFateResolved(uint32(s)) || status.IsFateHandled(uint32(s))
 }
 
-// IsResolved returns true, only if the fate of the promise is Handled.
+// IsHandled returns true, only if the fate of the promise is Handled.
 //
 // For more info, see 'States and Fates' in the package comment.
 func (s Status) IsHandled() bool {
