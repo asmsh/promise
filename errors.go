f61f16354e09e56a72d58d051c8bdf1c5585a1fb
Ahmad Sameh <8120013+asmsh@users.noreply.github.com>
2023-09-30 03:36:32 +0300

    BREAKING: changed the type of the UncaughtPanic and UncaughtError errors returned to not be of pointer types

--- errors.go
+++ errors.go
@@ -3,7 +3,6 @@ package promise
 import (
 	"errors"
 	"fmt"
-	"strings"
 )
 
 var (
@@ -23,44 +22,23 @@ var (
 // but hasn't been caught by the end of that chain.
 type UncaughtPanic struct{ v any }
 
-func (e *UncaughtPanic) Error() string {
+func (e UncaughtPanic) Error() string {
 	return fmt.Sprintf("uncaught panic in the promise chain: %v", e.v)
 }
 
-func (e *UncaughtPanic) Unwrap() error {
+func (e UncaughtPanic) Unwrap() error {
 	err, _ := e.v.(error)
 	return err
 }
 
-func (e *UncaughtPanic) V() any { return e.v }
-
-func newUncaughtPanic(v any) *UncaughtPanic { return &UncaughtPanic{v: v} }
+func (e UncaughtPanic) V() any { return e.v }
 
 // UncaughtError wraps an error that happened in a promise chain, but hasn't
 // been caught, by the end of that chain.
 type UncaughtError struct{ err error }
 
-func (e *UncaughtError) Error() string {
+func (e UncaughtError) Error() string {
 	return fmt.Sprintf("uncaught error in the promise chain: %s", e.err)
 }
 
-func (e *UncaughtError) Unwrap() error { return e.err }
-
-func newUncaughtError(err error) *UncaughtError { return &UncaughtError{err: err} }
-
-type wrapErrors struct{ errs []error }
-
-func (e *wrapErrors) Error() string {
-	b := strings.Builder{}
-	for i, err := range e.errs {
-		if i != 0 {
-			b.WriteString(": ")
-		}
-		b.WriteString(err.Error())
-	}
-	return b.String()
-}
-
-func (e *wrapErrors) Unwrap() []error { return e.errs }
-
-func newWrapErrs(errs ...error) *wrapErrors { return &wrapErrors{errs: errs} }
+func (e UncaughtError) Unwrap() error { return e.err }
