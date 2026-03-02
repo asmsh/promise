53bdf2183943d2a05f60779299f21e274808d196
Ahmad Sameh <8120013+asmsh@users.noreply.github.com>
2026-03-02 23:04:07 +0400

    docs: update the docs of the GroupConfig

--- group_options.go
+++ group_options.go
@@ -16,73 +16,81 @@ package promise
 
 import "context"
 
+// DefaultGroupConfig is the default [GroupConfig] value, equivalent to
+// a zero-value [Group].
+var DefaultGroupConfig = GroupConfig{}
+
+// GroupOption configures a [Group].
 type GroupOption func(core *groupCore)
 
+// ApplyConfig applies all the settings from the provided [GroupConfig]
+// value, conf, to the [Group].
 func ApplyConfig(conf GroupConfig) GroupOption {
 	return func(core *groupCore) {
-		if cb := conf.UnhandledPanicCB; cb != nil {
-			core.unhandledPanicCB = cb
+		Size(conf.Size)(core)
+
+		if conf.UnhandledPanicCB != nil {
+			UnhandledPanicCB(conf.UnhandledPanicCB)(core)
 		}
 
-		if cb := conf.UnhandledErrorCB; cb != nil {
-			core.unhandledErrorCB = cb
+		if conf.UnhandledErrorCB != nil {
+			UnhandledErrorCB(conf.UnhandledErrorCB)(core)
 		}
-
-		core.sg.SetSize(conf.Size)
 
 		if conf.OnetimeHandling {
-			core.options.SetOnetimeHandling()
+			OnetimeHandling()(core)
 		}
 
-		if conf.NeverCancelCBCtx && !conf.FailuresCancelCBCtx && !conf.FailuresCancelGroup {
-			core.options.SetNeverCancelCBCtx()
-		}
-
+		// Apply the cancel flags before NeverCancelCBCtx, because
+		// NeverCancelCBCtx checks if they're already set on the core.
 		if conf.FailuresCancelCBCtx {
-			core.ctx, core.cancel = context.WithCancel(context.Background())
-			core.options.SetFailuresCancelCBCtx()
-			core.options.ResetNeverCancelCBCtx()
+			FailuresCancelCBCtx()(core)
 		}
 
 		if conf.FailuresCancelGroup {
-			core.ctx, core.cancel = context.WithCancel(context.Background())
-			core.options.SetFailuresCancelGroup()
-			core.options.ResetNeverCancelCBCtx()
+			FailuresCancelGroup()(core)
+		}
+
+		if conf.NeverCancelCBCtx {
+			NeverCancelCBCtx()(core)
 		}
 
 		if conf.NoNilCtxDoneChan {
-			core.options.SetNoNilCtxDoneChan()
+			NoNilCtxDoneChan()(core)
 		}
 
 		if conf.NoWaitingBusyGroup {
-			core.options.SetNoWaitingBusyGroup()
+			NoWaitingBusyGroup()(core)
 		}
 
 		if conf.SaveAllGroupResults {
-			core.options.SetSaveAllGroupResults()
+			SaveAllGroupResults()(core)
 		}
 	}
 }
 
+// UnhandledPanicCB sets the [GroupConfig.UnhandledPanicCB] to the provided cb value.
 func UnhandledPanicCB(cb func(any)) GroupOption {
 	return func(core *groupCore) {
 		core.unhandledPanicCB = cb
 	}
 }
 
+// UnhandledErrorCB sets the [GroupConfig.UnhandledErrorCB] to the provided cb value.
 func UnhandledErrorCB(cb func(error)) GroupOption {
 	return func(core *groupCore) {
 		core.unhandledErrorCB = cb
 	}
 }
 
+// Size sets the [GroupConfig.Size] to the provided size value.
 func Size(size int) GroupOption {
 	return func(core *groupCore) {
 		core.sg.SetSize(size)
 	}
 }
 
-// OnetimeHandling sets the [GroupConfig.OnetimeHandling] value to true,
+// OnetimeHandling sets the [GroupConfig.OnetimeHandling] to true,
 // or to the set value provided, if any.
 func OnetimeHandling(set ...bool) GroupOption {
 	return func(core *groupCore) {
@@ -90,7 +98,37 @@ func OnetimeHandling(set ...bool) GroupOption {
 	}
 }
 
-// NeverCancelCBCtx sets the [GroupConfig.NeverCancelCBCtx] value to true,
+// FailuresCancelCBCtx sets the [GroupConfig.FailuresCancelCBCtx] to true,
+// or to the set value provided, if any.
+func FailuresCancelCBCtx(set ...bool) GroupOption {
+	return func(core *groupCore) {
+		if getEffSet(set...) {
+			core.ctx, core.cancel = context.WithCancel(context.Background())
+			core.options.SetNeverCancelCBCtxTo(false)
+			core.options.SetFailuresCancelCBCtxTo(true)
+		} else {
+			core.ctx, core.cancel = nil, nil
+			core.options.SetFailuresCancelCBCtxTo(false)
+		}
+	}
+}
+
+// FailuresCancelGroup sets the [GroupConfig.FailuresCancelGroup] to true,
+// or to the set value provided, if any.
+func FailuresCancelGroup(set ...bool) GroupOption {
+	return func(core *groupCore) {
+		if getEffSet(set...) {
+			core.ctx, core.cancel = context.WithCancel(context.Background())
+			core.options.SetNeverCancelCBCtxTo(false)
+			core.options.SetFailuresCancelGroupTo(true)
+		} else {
+			core.ctx, core.cancel = nil, nil
+			core.options.SetFailuresCancelGroupTo(false)
+		}
+	}
+}
+
+// NeverCancelCBCtx sets the [GroupConfig.NeverCancelCBCtx] to true,
 // or to the set value provided, if any.
 func NeverCancelCBCtx(set ...bool) GroupOption {
 	return func(core *groupCore) {
@@ -106,37 +144,7 @@ func NeverCancelCBCtx(set ...bool) GroupOption {
 	}
 }
 
-// FailuresCancelCBCtx sets the [GroupConfig.FailuresCancelCBCtx] value to true,
-// or to the set value provided, if any.
-func FailuresCancelCBCtx(set ...bool) GroupOption {
-	return func(core *groupCore) {
-		if getEffSet(set...) {
-			core.ctx, core.cancel = context.WithCancel(context.Background())
-			core.options.SetNeverCancelCBCtxTo(false)
-			core.options.SetFailuresCancelCBCtxTo(true)
-		} else {
-			core.ctx, core.cancel = nil, nil
-			core.options.SetFailuresCancelCBCtxTo(false)
-		}
-	}
-}
-
-// FailuresCancelGroup sets the [GroupConfig.FailuresCancelGroup] value to true,
-// or to the set value provided, if any.
-func FailuresCancelGroup(set ...bool) GroupOption {
-	return func(core *groupCore) {
-		if getEffSet(set...) {
-			core.ctx, core.cancel = context.WithCancel(context.Background())
-			core.options.SetNeverCancelCBCtxTo(false)
-			core.options.SetFailuresCancelGroupTo(true)
-		} else {
-			core.ctx, core.cancel = nil, nil
-			core.options.SetFailuresCancelGroupTo(false)
-		}
-	}
-}
-
-// NoNilCtxDoneChan sets the [GroupConfig.NoNilCtxDoneChan] value to true,
+// NoNilCtxDoneChan sets the [GroupConfig.NoNilCtxDoneChan] to true,
 // or to the set value provided, if any.
 func NoNilCtxDoneChan(set ...bool) GroupOption {
 	return func(core *groupCore) {
@@ -144,7 +152,7 @@ func NoNilCtxDoneChan(set ...bool) GroupOption {
 	}
 }
 
-// NoWaitingBusyGroup sets the [GroupConfig.NoWaitingBusyGroup] value to true,
+// NoWaitingBusyGroup sets the [GroupConfig.NoWaitingBusyGroup] to true,
 // or to the set value provided, if any.
 func NoWaitingBusyGroup(set ...bool) GroupOption {
 	return func(core *groupCore) {
@@ -152,7 +160,7 @@ func NoWaitingBusyGroup(set ...bool) GroupOption {
 	}
 }
 
-// SaveAllGroupResults sets the [GroupConfig.SaveAllGroupResults] value to true,
+// SaveAllGroupResults sets the [GroupConfig.SaveAllGroupResults] to true,
 // or to the set value provided, if any.
 func SaveAllGroupResults(set ...bool) GroupOption {
 	return func(core *groupCore) {
@@ -170,15 +178,25 @@ func getEffSet(set ...bool) bool {
 	return s
 }
 
+// GroupConfig holds the configuration settings for a [Group].
+// It can be applied to a [Group] by passing [ApplyConfig] to [NewGroup],
+// or by using the individual option functions directly.
 type GroupConfig struct {
-	UnhandledPanicCB func(v any)
-	UnhandledErrorCB func(err error)
-
 	// Size is the number of goroutines which this group is allowed to run.
 	// This includes goroutines created for both, constructor calls(Go, GoCtxRes, etc.),
 	// and follow calls([Promise.Then], [Promise.Catch], etc.).
 	// If it's 0 or less, then the group size is unlimited.
 	Size int
+
+	// UnhandledPanicCB is called with the panic value once any [Promise] in
+	// this [Group] resolves to [Panic] and it's not handled by any follow
+	// calls, like [Promise.Recover] or [Promise.Res].
+	UnhandledPanicCB func(v any)
+
+	// UnhandledErrorCB is called with the error value once any [Promise] in
+	// this [Group] resolves to [Error] and it's not handled by any follow
+	// calls, like [Promise.Catch] or [Promise.Res].
+	UnhandledErrorCB func(err error)
 
 	// OnetimeHandling enforces that the [Result] value returned from any
 	// callback is only used one-time (via [Promise.Res] or as an argument
@@ -186,17 +204,6 @@ type GroupConfig struct {
 	// This will cause any further attempt to use that [Result] value to
 	// return an [Error] [Result] with the [ErrPromiseConsumed] error.
 	OnetimeHandling bool
-
-	// NeverCancelCBCtx will result in passing a never canceled [context.Context]
-	// value to all callbacks.
-	//
-	// If [GroupConfig.FailuresCancelCBCtx] or [GroupConfig.FailuresCancelGroup]
-	// is true, this will be ignored.
-	//
-	// The default behavior is always canceling the callback's [context.Context]
-	// value when its respective [Promise] returns, that is when the callback
-	// function itself returns.
-	NeverCancelCBCtx bool
 
 	// FailuresCancelCBCtx will result in canceling all [context.Context] values
 	// passed to all callbacks, once any callback returns an [Error] or a [Panic]
@@ -234,6 +241,17 @@ type GroupConfig struct {
 	//
 	// Inspired from: https://github.com/golang/go/issues/54045
 	FailuresCancelGroup bool
+
+	// NeverCancelCBCtx will result in passing a never canceled [context.Context]
+	// value to all callbacks.
+	//
+	// If [GroupConfig.FailuresCancelCBCtx] or [GroupConfig.FailuresCancelGroup]
+	// is true, this will be ignored.
+	//
+	// The default behavior is always canceling the callback's [context.Context]
+	// value when its respective [Promise] returns, that is when the callback
+	// function itself returns.
+	NeverCancelCBCtx bool
 
 	// NoNilCtxDoneChan causes new calls to [Group.Ctx] to return an [Error] [Result]
 	// with the [ErrNilCtxDone] err, when the [context.Context] value passed
